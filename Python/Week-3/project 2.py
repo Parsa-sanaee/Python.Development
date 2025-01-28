d6fb4375a79a2361e22386754f8ed5a33e31e420
Parsa-XD <parsasanaee88@gmail.com>
2025-01-28 10:49:03 +0330

    Week 3 py class

--- Python/Week-3/project 2.py
+++ Python/Week-3/project 2.py
@@ -1,18 +1,7 @@
-numbers = input("Enter a list of numbers separated by spaces: ").split()
-numbers = [int(num) for num in numbers]
+lst = list(map(int, input("Please enter a list of numbers separated by spaces: ").split()))
+n = int(input("Enter the number of rotations: "))
 
-rotations = int(input("Enter the number of rotations: "))
+# Simple list slicing for rotation
+result = lst[-n:] + lst[:-n]
 
-rotations = rotations % len(numbers)
-
-numbers.reverse()
-
-first_part = numbers[:len(numbers) - rotations]
-first_part.reverse()
-
-second_part = numbers[len(numbers) - rotations:]
-second_part.reverse()
-
-rotated_list = first_part + second_part
-
-print("Rotated list:", rotated_list)
+print(result)
